fd26e4ba21367b647ab7b22f70a5df712c5999b4
Nobody4931 <ardenxie1@gmail.com>
2021-07-05 10:23:06 -0400

    might flex on randy again

--- src/main.js
+++ src/main.js
@@ -11,12 +11,14 @@ Dispatcher.AddHandler("READY", async () => {
 
 	await Memory.CollectClient();
 	await Memory.CollectGuild();
+	await Memory.CollectEmojis();
 
 	let Owner = await Memory.CollectUser((User) => (User.ID == Options.OwnerID));
 
 	console.log(`Successfully authenticated as: ${Memory.Client.Tag} (${Memory.Client.ID})\n`);
-	console.log(`Found guild to manage: ${Memory.Guild.Name} (${Memory.Guild.ID})`);
-	console.log(`Found owner: ${Owner.Tag} (${Owner.ID})\n`);
+	console.log(`Found guild: ${Memory.Guild.Name} (${Memory.Guild.ID})`);
+	console.log(`Found owner: ${Owner.Tag} (${Owner.ID})`);
+	console.log(`Found ${Object.keys(Memory.Emojis).length - 1} emojis\n`);
 	console.log(`Took ${EndTime - StartTime} ms`);
 });
 
