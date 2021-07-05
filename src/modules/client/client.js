527305e78e65e39c4a11007cecd687732dd36880
Nobody4931 <ardenxie1@gmail.com>
2021-07-04 22:47:37 -0400

    ngl completely forgot what i changed here

--- src/modules/client/client.js
+++ src/modules/client/client.js
@@ -2,15 +2,14 @@
 import * as DAPI from "../api/api.js";
 import * as HttpOP from "../api/enums/http.js";
 
-export async function LeaveGuild(GuildID) {
-	let Response = await DAPI.DELETERequestA(`users/@me/guilds/${GuildID}`);
+export async function LeaveGuild(GuildID, Data, Headers) {
+	let Response = await DAPI.DELETERequestA(`users/@me/guilds/${GuildID}`, Data, Headers);
 	if (Response.status != HttpOP.NO_CONTENT) throw `HTTP_ERROR: ${Response.statusText}`;
 	return Response;
 }
 
-export async function CreateDM(RecipientID) {
-	let Response = await DAPI.POSTRequestA("users/@me/channels", {
-		recipient_id: RecipientID });
+export async function CreateDM(Data, Headers) {
+	let Response = await DAPI.POSTRequestA("users/@me/channels", Data, Headers);
 	if (Response.status != HttpOP.OK) throw `HTTP_ERROR: ${Response.statusText}`;
 	return Response;
 }
