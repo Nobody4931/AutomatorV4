6aebd4020365ccd162504cbf26ed1825a71f6ffc
Nobody4931 <ardenxie1@gmail.com>
2021-07-05 13:18:43 -0400

    cleanup entry point

--- src/main.js
+++ src/main.js
@@ -3,24 +3,18 @@ import * as Socket from "./modules/api/socket.js";
 import * as Memory from "./modules/client/memory.js";
 import * as Dispatcher from "./modules/client/dispatcher.js";
 
-let StartTime;
-let EndTime;
-
 Dispatcher.AddHandler("READY", async () => {
 	await Memory.CollectClient();
 	await Memory.CollectGuild();
 	await Memory.CollectEmojis();
 
 	let Owner = await Memory.CollectUser((User) => (User.ID == Options.OwnerID));
-
-	EndTime = performance.now();
+	let Emojis = Object.keys(Memory.Emojis);
 
 	console.log(`Successfully authenticated as: ${Memory.Client.Tag} (${Memory.Client.ID})\n`);
 	console.log(`Found guild: ${Memory.Guild.Name} (${Memory.Guild.ID})`);
 	console.log(`Found owner: ${Owner.Tag} (${Owner.ID})`);
-	console.log(`Found ${Object.keys(Memory.Emojis).length} emojis\n`);
-	console.log(`Took ${EndTime - StartTime} ms`);
+	console.log(`Found ${Emojis.length} emojis: ${Emojis.join(", ")}\n`);
 });
 
-StartTime = performance.now();
 Socket.Connect();
