8f8a001379915b438f5a8ae452c4af4725cf6338
Nobody4931 <ardenxie1@gmail.com>
2021-07-05 00:11:22 -0400

    some twitter snowflake (de)serializer

---
+++ src/modules/api/snowflake.js
@@ -0,0 +1,10 @@
+export const Epoch  = new Date(1970, 0).getTime();
+export const DEpoch = new Date(2015, 0).getTime();
+
+export function FromDate(Time) {
+	return (BigInt(Time.getTime() - DEpoch) << 22n).toString();
+}
+
+export function ToDate(Snowflake) {
+	return new Date(Number( (BigInt(Snowflake) >> 22n) + BigInt(DEpoch) ));
+}
