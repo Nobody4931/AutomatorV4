512294aef2369b00af7c68b9c5da14137a8d3b38
Nobody4931 <ardenxie1@gmail.com>
2021-07-05 00:08:19 -0400

    successfully flexed on randy

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import * as Socket from "./modules/api/socket.js";
-import * as DAPI from "./modules/api/api.js";
+import * as Dispatcher from "./modules/client/dispatcher.js";
 
+let StartingTime = null;
+
+Dispatcher.AddHandler("READY", async () => {
+	let EndingTime = performance.now();
+
+	console.log(`client is ready`);
+	console.log(`took ${EndingTime - StartingTime} ms`);
+});
+
+console.log("starting...");
+
+StartingTime = performance.now();
 Socket.Connect();
