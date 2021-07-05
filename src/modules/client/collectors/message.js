bfbd54cef7736ed15f59a88fb77e6bc4f63d1380
Nobody4931 <ardenxie1@gmail.com>
2021-07-05 15:39:15 -0400

    couldnt escape oop god dammit

--- src/modules/client/collectors/message.js
+++ src/modules/client/collectors/message.js
@@ -1,36 +1,14 @@
+import * as BaseCollector from "./base.js";
+
 export const ActiveC = [];
 export const ActiveF = [];
 
 /* Collector Class */
-export class Collector {
+export class Collector extends BaseCollector.LiveCollector {
 	constructor(Filter, Options = {}) {
-		this.Started = false;
-		this.Ended = false;
-		this.Filter = Filter;
-		this.Limit = Options.Limit;
-		this.Duration = Options.Duration;
-		this.CollCallback = null;
-		this.StopCallback = null;
-		this.Timer = null;
+		super(Filter, Options);
 
 		ActiveC.push(this);
-	}
-
-	OnCollect(Callback) {
-		this.CollCallback = Callback;
-	}
-
-	OnStop(Callback) {
-		this.StopCallback = Callback;
-	}
-
-	Start() {
-		this.Started = true;
-		this.Ended = false;
-
-		if (this.Duration != null) {
-			this.Timer = setTimeout(this.Stop.bind(this), this.Duration);
-		}
 	}
 
 	Stop(_Index) {
@@ -38,33 +16,23 @@ export class Collector {
 		this.Ended = true;
 
 		ActiveC.splice(_Index ?? ActiveC.findIndex((V) => (V == this)), 1);
-
-		if (this.Timer != null)
-			clearTimeout(this.Timer);
-		if (this.StopCallback != null)
-			this.StopCallback();
+		super.Stop();
 	}
 };
 
 /* Collector Function */
-export async function Collect(Filter, Options) {
+export async function Collect(Filter, Options = {}) {
 	return new Promise((Resolve) => {
-		let CollectionTask = {};
-		CollectionTask.Collected = [];
-		CollectionTask.Filter = Filter;
-		CollectionTask.Limit = Options.Limit;
-		CollectionTask.Timer = null;
+		let Collector = new BaseCollector.BulkCollector(Filter, {
+			Limit: Options.Limit,
+			Duration: Options.Duration,
+			Callback: (Collected, _Index) => {
+				ActiveF.splice(_Index ?? ActiveF.findIndex((V) => (V == Collector)), 1);
+				console.log(ActiveF);
+				Resolve(Collected);
+			}
+		});
 
-		CollectionTask.Stop = function(_Index) {
-			if (CollectionTask.Timer != null)
-				clearTimeout(CollectionTask.Timer);
-			ActiveF.splice(_Index ?? ActiveF.findIndex((V) => (V == CollectionTask)), 1);
-			Resolve(CollectionTask.Collected);
-		}
-
-		if (Options.Duration != null)
-			CollectionTask.Timer = setTimeout(CollectionTask.Stop, Options.Duration);
-
-		ActiveF.push(CollectionTask);
+		ActiveF.push(Collector);
 	});
 }
