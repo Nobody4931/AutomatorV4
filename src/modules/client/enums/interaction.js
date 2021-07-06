de4c60bc71c88a0371f763b30a421268ed59e70f
Nobody4931 <ardenxie1@gmail.com>
2021-07-06 15:04:03 -0400

    interaction enums

---
+++ src/modules/client/enums/interaction.js
@@ -0,0 +1,17 @@
+// Interaction Type
+// https://discord.com/developers/docs/interactions/slash-commands#interaction-object-interaction-request-type
+export const PING                = 1;
+export const APPLICATION_COMMAND = 2;
+export const MESSAGE_COMPONENT   = 3;
+
+// Interaction Callback Type
+// https://discord.com/developers/docs/interactions/slash-commands#interaction-response-object-interaction-callback-type
+export const CB_PONG                                 = 1;
+export const CB_CHANNEL_MESSAGE_WITH_SOURCE          = 4;
+export const CB_DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5;
+export const CB_DEFERRED_UPDATE_MESSAGE              = 6;
+export const CB_UPDATE_MESSAGE                       = 7;
+
+// Interaction Callback Flags
+// https://discord.com/developers/docs/interactions/slash-commands#interaction-response-object-interaction-application-command-callback-data-flags
+// [!] Same as message flags for now, check back later to see if it changes
