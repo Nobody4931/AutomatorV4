4862776702a6ab5cfe3e873ddf244d8c73c63cf1
Nobody4931 <ardenxie1@gmail.com>
2021-07-06 18:52:01 -0400

    thank you javascript, very cool

--- src/modules/client/wrappers/images.js
+++ src/modules/client/wrappers/images.js
@@ -1,3 +1,3 @@
 // https://discord.com/developers/docs/reference#image-formatting
-export const GetAvatarURL = (UserID, Hash, Format = "png", Size = 256)
-	=> `https://cdn.discordapp.com/avatars/${UserID}/${Hash}.${Format}?size=${Size}`;
+export const GetAvatarURL = (UserID, Hash, Format = "png", Size = 256) =>
+	`https://cdn.discordapp.com/avatars/${UserID}/${Hash}.${Format}?size=${Size}`;
