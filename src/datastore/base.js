a50acbd3d5655ae99821ac55dd7c02ff3aa0faba
Nobody4931 <ardenxie1@gmail.com>
2021-07-11 23:29:33 -0400

    file formatter finished

--- src/datastore/base.js
+++ src/datastore/base.js
@@ -1,8 +1,67 @@
 import * as Mask from "./mask.js";
 import Fs from "fs";
 
-export class AutoFormat {
-	constructor(Structure, Array = false) {
-		;
+export default class AutoFormat {
+	constructor(FilePath, Structure, Array = false) {
+		this.FilePath = FilePath;
+		this.Struct = Structure;
+		this.IsIter = Array;
+		this.Data = null;
+	}
+
+	Load() {
+		if (this.Data != null) return this;
+		if (this.IsIter == true)
+			this.Data = [];
+
+		if (!Fs.existsSync(this.FilePath))
+			return this;
+
+		let FileData = Buffer.from(Fs.readFileSync(this.FilePath, { encoding: "binary" }), "binary");
+		let Offset = 64;
+
+		Mask.Apply(FileData);
+
+		if (this.IsIter == true) {
+			while (Offset < FileData.byteLength) {
+				let ElementData = this.Struct.Read(FileData, Offset); Offset = ElementData[1];
+				this.Data.push(ElementData[0]);
+			}
+		}
+
+		else {
+			this.Data = this.Struct.Read(FileData, Offset)[0];
+		}
+
+		return this;
+	}
+
+	Save() {
+		let Bytes = 64; // wtf nice formatting
+		if (this.IsIter == true) for (const Element of this.Data)
+			Bytes += this.Struct.Calculate(Element);
+		else
+			Bytes += this.Struct.Calculate(this.Data);
+
+		let FileData = Buffer.alloc(Bytes);
+		let Offset = 64;
+
+		Mask.Generate(FileData);
+
+		if (this.IsIter == true) for (const Element of this.Data) {
+			Offset = this.Struct.Write(FileData, Element, Offset);
+		}
+
+		else {
+			Offset = this.Struct.Write(FileData, this.Data, Offset);
+		}
+
+		Mask.Apply(FileData);
+
+		if (Fs.existsSync(this.FilePath))
+			Fs.renameSync(this.FilePath, `${this.FilePath}.old`);
+		Fs.writeFileSync(this.FilePath, FileData, { encoding: "binary" });
+
+		return this;
 	}
 };
