54594f1b4c685fc1731d0fd8fc0aea0b4b03b77f
Nobody4931 <ardenxie1@gmail.com>
2021-07-11 21:35:08 -0400

    yeah who needs this lol

--- src/datastore/enums/users.js
+++
@@ -1,7 +0,0 @@
-// Datavalue Type
-export const NAME      = 1;
-export const SCHOOL    = 2;
-export const GRAD_YEAR = 3;
-export const MODIFIERS = 4;
-export const RANK      = 5;
-export const COLOR     = 6;
