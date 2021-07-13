c01817cd3052883ea6cea818d9563baf90476994
Nobody4931 <ardenxie1@gmail.com>
2021-07-12 22:33:34 -0400

    yeah i cant read apparently

--- src/modules/api/snowflake.js
+++ src/modules/api/snowflake.js
@@ -2,9 +2,9 @@ export const Epoch  = new Date(1970, 0).getTime();
 export const DEpoch = new Date(2015, 0).getTime();
 
 export function FromDate(Time) {
-	return (BigInt(Time.getTime() - DEpoch) << 22n).toString();
+	return BigInt(Time.getTime() - DEpoch) << 22n;
 }
 
 export function ToDate(Snowflake) {
-	return new Date(Number( (BigInt(Snowflake) >> 22n) + BigInt(DEpoch) ));
+	return new Date(Number( (Snowflake >> 22n) + BigInt(DEpoch) ));
 }
