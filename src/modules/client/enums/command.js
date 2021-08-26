dd0830c5e63c6b501eb91aac3f012c246f4fa1a8
Nobody4931 <ardenxie1@gmail.com>
2021-08-26 18:49:53 -0400

    update application commands enums

--- src/modules/client/enums/command.js
+++ src/modules/client/enums/command.js
@@ -1,11 +1,23 @@
+// Application Command Type
+// https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types
+export const CHAT_INPUT = 1;
+export const USER       = 2;
+export const MESSAGE    = 3;
+
 // Command Option Type
-// https://discord.com/developers/docs/interactions/slash-commands#application-command-object-application-command-option-type
-export const SUB_COMMAND       = 1;
-export const SUB_COMMAND_GROUP = 2;
-export const STRING            = 3;
-export const INTEGER           = 4;
-export const BOOLEAN           = 5;
-export const USER              = 6;
-export const CHANNEL           = 7;
-export const ROLE              = 8;
-export const MENTIONABLE       = 9;
+// https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
+export const COPT_SUB_COMMAND       = 1;
+export const COPT_SUB_COMMAND_GROUP = 2;
+export const COPT_STRING            = 3;
+export const COPT_INTEGER           = 4;
+export const COPT_BOOLEAN           = 5;
+export const COPT_USER              = 6;
+export const COPT_CHANNEL           = 7;
+export const COPT_ROLE              = 8;
+export const COPT_MENTIONABLE       = 9;
+export const COPT_NUMBER            = 10;
+
+// Command Permission Type
+// https://discord.com/developers/docs/interactions/application-commands#application-command-permissions-object-application-command-permission-type
+export const CPRM_ROLE = 1;
+export const CPRM_USER = 2;
