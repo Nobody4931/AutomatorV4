cd266f24be849782df77678f16ba0435ddf47b6a
Nobody4931 <ardenxie1@gmail.com>
2021-08-26 19:17:50 -0400

    update command type field

--- src/commands/help/command.js
+++ src/commands/help/command.js
@@ -1,6 +1,8 @@
+import * as CommandType from "../../modules/client/enums/command.js";
 import HelpCmd from "./default.js";
 
 export const Structure = {
+	["type"]: CommandType.CHAT_INPUT,
 	["name"]: "help",
 	["description"]: "Displays information about the bot",
 	["default_permission"]: true,
--- src/commands/version/command.js
+++ src/commands/version/command.js
@@ -1,6 +1,8 @@
+import * as CommandType from "../../modules/client/enums/command.js";
 import VersionCommand from "./default.js";
 
 export const Structure = {
+	["type"]: CommandType.CHAT_INPUT,
 	["name"]: "version",
 	["description"]: "Displays the version the bot is currently running on",
 	["default_permission"]: true,
